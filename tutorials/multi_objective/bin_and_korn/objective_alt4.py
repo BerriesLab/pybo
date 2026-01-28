c4a5fcf4da4a1cc45336dae2cd228f9ed6480c5f
BerettaDavide <mail.davide.beretta@gmail.com>
2026-01-28 18:51:02 +0100

    obj alt

--- tutorials/multi_objective/bin_and_korn/objective_alt4.py
+++ tutorials/multi_objective/bin_and_korn/objective_alt4.py
@@ -9,28 +9,6 @@ class BinhAndKorn(MCMultiObjectiveBase):
     def __init__(self, device: torch.device, dtype: torch.dtype):
         super().__init__(device=device, dtype=dtype)
 
-    def _f1(self, X: torch.Tensor) -> torch.Tensor:
-        x1 = X[..., self.par_cfg[0].index]
-        x2 = X[..., self.par_cfg[1].index]
-        return 4 * x1 ** 2 + 4 * x2 ** 2
-
-    def _f2(self, X: torch.Tensor) -> torch.Tensor:
-        x1 = X[..., self.Par.P1.index]
-        x2 = X[..., self.Par.P2.index]
-        return (x1 - 5) ** 2 + (x2 - 5) ** 2
-
-    def _input_c1(self, X: torch.Tensor) -> torch.Tensor:
-        """ A constraint on the input: (x0 - 5)^2 + x1^2 <= 25 """
-        x1 = X[..., self.Par.P1.index]
-        x2 = X[..., self.Par.P2.index]
-        return 25 - ((x1 - 5) ** 2 + x2 ** 2)
-
-    def _input_c2(self, X: torch.Tensor) -> torch.Tensor:
-        """ A constraint on the input: (x0 - 8)^2 + (x1 + 3)^2 >= 7.7 """
-        x1 = X[..., self.Par.P1.index]
-        x2 = X[..., self.Par.P2.index]
-        return (x1 - 8) ** 2 + (x2 + 3) ** 2 - 7.7
-
     def evaluate_true_objective(self, X: torch.Tensor, add_noise=False) -> torch.Tensor:
         f1 = self._f1(X=X)
         f2 = self._f2(X=X)
